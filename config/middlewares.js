ee4d55a06ac264cfc21883f923978d1838c55414
Robert Romulus <digitalmediaflow@gmail.com>
2023-07-28 06:05:14 +0000

    updated cors

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,25 +1,33 @@
-module.exports = [
-  'strapi::errors',
-  {
-    name: 'strapi::security',
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          'connect-src': ["'self'", 'https:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-          upgradeInsecureRequests: null,
+module.exports = ({ env }) => ({
+  settings: {
+    cors: {
+      enabled: true,
+      origin: '*', // Allow requests from any domain
+    },
+  },
+  middleware: [
+    'strapi::errors',
+    {
+      name: 'strapi::security',
+      config: {
+        contentSecurityPolicy: {
+          useDefaults: true,
+          directives: {
+            'connect-src': ["'self'", 'https:'],
+            'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+            'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+            upgradeInsecureRequests: null,
+          },
         },
       },
     },
-  },
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-];
+    'strapi::cors', // CORS middleware is already in the original code
+    'strapi::poweredBy',
+    'strapi::logger',
+    'strapi::query',
+    'strapi::body',
+    'strapi::session',
+    'strapi::favicon',
+    'strapi::public',
+  ],
+});
