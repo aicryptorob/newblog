41e631ffe44946ece3c0f5c48c711f9596c425ab
Robert Romulus <digitalmediaflow@gmail.com>
2023-07-28 06:15:40 +0000

    reverted cors

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,33 +1,25 @@
-module.exports = ({ env }) => ({
-  settings: {
-    cors: {
-      enabled: true,
-      origin: '*', // Allow requests from any domain
-    },
-  },
-  middleware: [
-    'strapi::errors',
-    {
-      name: 'strapi::security',
-      config: {
-        contentSecurityPolicy: {
-          useDefaults: true,
-          directives: {
-            'connect-src': ["'self'", 'https:'],
-            'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-            'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-            upgradeInsecureRequests: null,
-          },
+module.exports = [
+  'strapi::errors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          upgradeInsecureRequests: null,
         },
       },
     },
-    'strapi::cors', // CORS middleware is already in the original code
-    'strapi::poweredBy',
-    'strapi::logger',
-    'strapi::query',
-    'strapi::body',
-    'strapi::session',
-    'strapi::favicon',
-    'strapi::public',
-  ],
-});
+  },
+  'strapi::cors', 
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
